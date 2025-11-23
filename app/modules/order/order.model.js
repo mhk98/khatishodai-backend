37ec23f7c8be5ec972952a69019dedf82ae4f2d2
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2025-11-23 22:36:44 +0600

    now order without login

--- app/modules/order/order.model.js
+++ app/modules/order/order.model.js
@@ -1,100 +1,3 @@
-// module.exports = (sequelize, DataTypes) => {
-//   const Order = sequelize.define(
-//     "Order",
-//     {
-//       id: {
-//         type: DataTypes.STRING, // use STRING, not INTEGER
-//         primaryKey: true,
-//       },
-//       cartProducts: {
-//         type: DataTypes.JSON,
-//         allowNull: false,
-//         defaultValue: {},
-//         get() {
-//           return JSON.parse(this.getDataValue("cartProducts"));
-//         },
-//         set(val) {
-//           this.setDataValue("cartProducts", JSON.stringify(val));
-//         },
-//       },
-//       firstName: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//       },
-//       lastName: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//       },
-//       address: {
-//         type: DataTypes.TEXT,
-//         allowNull: true,
-//       },
-//       city: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//       },
-//       paymentMethod: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//       },
-//       orderStatus: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         defaultValue: "Processing",
-//       },
-//       emailOrPhone: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         validate: {
-//           notEmpty: true,
-//         },
-//       },
-//       postalCode: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//       },
-//       keepUpdate: {
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//         defaultValue: false,
-//       },
-//       saveInfo: {
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//         defaultValue: false,
-//       },
-//       total: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//       },
-//     },
-//     {
-//       timestamps: true,
-//       hooks: {
-//         // Generate custom ID before creating
-//         async beforeCreate(order) {
-//           const lastOrder = await Order.findOne({
-//             order: [["createdAt", "DESC"]],
-//           });
-
-//           let nextNumber = 1;
-//           if (lastOrder && lastOrder.id) {
-//             const lastNumber = parseInt(lastOrder.id.replace("KS", ""), 10);
-//             if (!isNaN(lastNumber)) {
-//               nextNumber = lastNumber + 1;
-//             }
-//           }
-
-//           order.id = "KS" + String(nextNumber).padStart(3, "0");
-//         },
-//       },
-//     }
-//   );
-
-//   return Order;
-// };
-
-
 module.exports = (sequelize, DataTypes) => {
   const Order = sequelize.define(
     "Order",
@@ -159,7 +62,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       discount: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
       },
       subTotal: {
         type: DataTypes.INTEGER,
